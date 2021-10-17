3fbce33cda94d065ce15a4ab3cf2ae7ff3760935
KobayashiMakoto9331 <kobax0@example.com>
2021-10-17 12:08:14 +0000

    ADD：モデルプレゼンターとフォームプレゼンターの追加

--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -1,3 +1,17 @@
 class StaffMemberPresenter < ModelPresenter
-  
+  delegate :suspended?, to: :object
+  #職員の停止フラグのOn/Offを表現する記号を返す。
+  #On: BALLOT_BOX_WITH_CHECK (U+2611)
+  #Off: BALLOT_BOX (U+2610)
+  def suspended_mark
+    object.suspended? ? raw("&#x2611;") : raw("&#x2610;")
+  end
+
+  def full_name
+    object.family_name + " " + object.given_name
+  end
+
+  def full_name_kana
+    object.family_name_kana + " " + object.given_name_kana
+  end
 end
