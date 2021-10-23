d911eba1e0da429862b71e527c7149db3a7d23d0
KobayashiMakoto9331 <kobax0@example.com>
2021-10-23 02:02:52 +0000

    ADD:プレゼンターで顧客CRUD実装

---
+++ app/forms/staff/customer_form.rb
@@ -0,0 +1,52 @@
+class Staff::CustomerForm
+  include ActiveModel::Model
+
+  attr_accessor :customer
+  delegate :persisted?, to: :customer #persistedメソッドをcustomerに移譲、persisted? -> customer.persisted?
+
+  def initialize(customer=nil)
+    @customer = customer
+    @customer ||= Customer.new(gender: "male")
+    @customer.build_home_address unless @customer.home_address #formを表示させるために記述。 has_one 記入で追加されたメソッド
+    @customer.build_work_address unless @customer.work_address
+  end
+
+  def assign_attributes(params={})
+    @params = params
+
+    customer.assign_attributes(customer_params)
+    customer.home_address.assign_attributes(home_address_params)
+    customer.work_address.assign_attributes(work_address_params)
+  end
+
+  def save
+    ActiveRecord::Base.transaction do
+      customer.save!
+      customer.home_address.save!
+      customer.work_address.save!
+    end
+  end
+
+  private
+
+  def customer_params
+    @params.require(:customer).permit(
+      :email, :password,
+      :family_name, :given_name, :family_name_kana, :given_name_kana,
+      :birthday, :gender
+    )
+  end
+
+  def home_address_params
+    @params.required(:home_address).permit(
+      :postal_code, :prefecture, :city, :address1, :address2,
+    )
+  end
+
+  def work_address_params
+    @params.required(:work_address).permit(
+    :company_name, :division_name,  :postal_code, :prefecture, :city, :address1, :address2,
+    )
+  end
+
+end
